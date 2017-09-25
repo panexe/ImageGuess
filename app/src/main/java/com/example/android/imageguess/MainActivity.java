804a74efeb415ef257052182bdae866547f303e3
panexe <larslpt@gmail.com>
2017-09-25 21:50:17 +0200

    OnClickListener Basics

--- app/src/main/java/com/example/android/imageguess/MainActivity.java
+++ app/src/main/java/com/example/android/imageguess/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.android.imageguess;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.view.View.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +18,25 @@ public class MainActivity extends AppCompatActivity {
     List<Button> button_letters = new ArrayList<>();
     List<Button> button_infos = new ArrayList<>();
 
+    LinearLayout LL_ViewButtons;
+
     ImageView Anzeige;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents();
+
+        for(int i = 0; i< 10; i++)
+        {
+            button_views.get(i).setOnClickListener(clickListener);
+            button_letters.get(i).setOnClickListener(clickListener);
+        }
+
     }
+
 
 
     private void initComponents()
@@ -55,9 +71,48 @@ public class MainActivity extends AppCompatActivity {
 
         Anzeige = (ImageView) findViewById(R.id.imageview);
 
+        LL_ViewButtons = (LinearLayout) findViewById(R.id.LinearLayout_ViewButtons);
 
-
+        LL_ViewButtons.setWeightSum(10);
 
 
     }
+
+    private OnClickListener clickListener = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            for (int i = 0; i < 10; i++) {
+                if (view == button_letters.get(i))
+                {
+                    getFirstEmptyView().setText(button_letters.get(i).getText());
+                    LL_ViewButtons.setWeightSum(3);
+                }
+                if (button_letters.get(i) == view)
+                {
+                    button_letters.get(i).setText("hallo");
+                    LL_ViewButtons.setWeightSum(3);
+                }
+
+
+            }
+        }
+    };
+
+
+    private Button getFirstEmptyView()
+    {
+        for (int i = 0 ;i < 10;i++)
+        {
+            if(button_views.get(i).getText() == "")
+            {
+                return button_views.get(i);
+            }
+
+
+        }
+        return null;
+    }
+
 }
+
+
---
+++ app/src/main/java/com/example/android/imageguess/Question.java
@@ -0,0 +1,15 @@
+package com.example.android.imageguess;
+
+/**
+ * Created by Lars on 25.09.2017.
+ */
+
+public class Question {
+    String Name;
+
+
+    public Question(String _name)
+    {
+        Name = _name;
+    }
+}
